da04be92f44ceec780f7b48922f091e74f990b57
saitovmarat <maratsajtov@gmail.com>
2024-12-14 23:56:05 +0300

    removed unnecessary function

--- 2_Lab/main.cpp
+++ 2_Lab/main.cpp
@@ -3,22 +3,17 @@
 #include "methods/Hook-JeevesMethod.cpp"
 #include "methods/NewtonMethod.cpp"
 
-double func1(const Point& point) {
+double func(const Point& point) {
   const double x1 = point.coords[0];
   const double x2 = point.coords[1];
   return 2 * pow(x1 - 1, 2) + pow(x2 - 6, 2);
 }
 
-double func2(const Point& point) {
-  const double x1 = point.coords[0];
-  const double x2 = point.coords[1];
-  return x1 * x1 + x2 * x2 - 6 * x1 - 3 * x2 + 5;
-}
 
 int main()
 {
-  HookJeevesMethod(func2).outputResults();
-  NewtonMethod(func2).outputResults();
+  HookJeevesMethod(func).outputResults();
+  NewtonMethod(func).outputResults();
 
   return 0;
 }
