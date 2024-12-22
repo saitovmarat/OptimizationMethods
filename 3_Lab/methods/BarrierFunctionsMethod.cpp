fbba4b03cace0cf609bd92057a5933c80b53b763
saitovmarat <maratsajtov@gmail.com>
2024-12-22 11:29:57 +0300

    [add] barrier functions method implementation

--- 3_Lab/methods/BarrierFunctionsMethod.cpp
+++ 3_Lab/methods/BarrierFunctionsMethod.cpp
@@ -1,27 +1,64 @@
 #include <functional>
-#include "../variables.h"
 #include <iostream>
+#include <iomanip>
+#include "../variables.h"
+#include "../helpfulFunctions.cpp"
 
 class BarrierFunctionsMethod
 {
 public:
-  BarrierFunctionsMethod(const std::function<double(Point)>& func) 
+  BarrierFunctionsMethod(
+    const std::function<double(Point)>& func, 
+    const std::function<std::vector<double>(Point)>& areaRestrictions) 
   {
     this->func = func;
-  };
-
-  void outputResults()
-  {
-    std::cout << "------------------------------------\n";
-    std::cout << "2) Метод барьерных функций\n";
-    std::cout << "Пока не реализовано\n";
+    this->areaRestrictions = areaRestrictions;
   }
+
+  void outputResults() {
+    std::cout << "------------------------------------\n";
+    std::cout << "2) Метод барьерных функций\n"; 
+    std::cout << "-----+------------+------------\n";
+    std::cout << std::setw(4) << "k" << " | " << std::setw(10) << "x1" << " | " << std::setw(10) << "x2" << "\n";
+    std::cout << "-----+------------+------------\n";
+    const std::pair<Point, double> methodResult = result();
+    std::cout << "-----+------------+------------\n";
+    std::cout << "Точка минимума X: " << "[" << methodResult.first.coords[0] << "; " << methodResult.first.coords[1] << "]" << "\n";
+    std::cout << "Значение модуля функции ошибки в этой точке = " << methodResult.second << "\n\n";
+  };    
 
 private:
   std::function<double(Point)> func;
+  std::function<std::vector<double>(Point)> areaRestrictions;
 
-  double result() const {
+private:
+  const std::pair<Point, double> result() const {
+    const double M = 1000;
+    Point basePoint = variables::START_POINT;
+    double ri = variables::R_02;
 
-    return 0.0;
+    for(int k = 1; k <= M; k++) {
+      const std::vector<double> grad_f_x = { 
+        helpfulFunctions::getFirstDerivative(func, basePoint, 1), 
+        helpfulFunctions::getFirstDerivative(func, basePoint, 2)
+      };
+      const vectorMatrix H_x = helpfulFunctions::getHesseMatrix(func, basePoint);
+      const vectorMatrix inversed_H_x = helpfulFunctions::getInversedMatrix(H_x);
+
+      const std::vector<double> area = areaRestrictions(basePoint);
+      double penalty = -1 * ri * helpfulFunctions::reversePenaltyFunc(area);
+      
+      if(abs(penalty) < variables::EPS) {
+        return std::make_pair(basePoint, abs(penalty));
+      }
+
+      basePoint -= helpfulFunctions::mulptiplyMatrixByVector(inversed_H_x, grad_f_x) + penalty;
+      
+      std::cout << std::setw(4) << k << " | " << std::setw(10) << basePoint.coords[0] << " | " << std::setw(10) << basePoint.coords[1] << "\n";
+
+      ri /= variables::C2;
+    }  
+    throw std::runtime_error("Метод Штрафов не сходится");
   }
+
 };
